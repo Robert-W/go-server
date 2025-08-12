da0e624b85af2243f133c662472728c26c3162e8
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-08-12 15:31:24 -0400

    refactor: committing routes now, committed response changes a minute ago

--- internal/routes/v1/sample/routes.go
+++ internal/routes/v1/sample/routes.go
@@ -2,12 +2,11 @@ package sample
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/robert-w/go-server/internal/db/services"
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
 func RegisterRoutes(subrouter *mux.Router, tracer oteltrace.Tracer) {
-	sampleHandler := &handler{service: &services.SampleService{}, tracer: tracer}
+	sampleHandler := &handler{service: &sampleService{}, tracer: tracer}
 	subrouter.HandleFunc("/samples", sampleHandler.listSamples).Methods("GET")
 	subrouter.HandleFunc("/samples", sampleHandler.createSamples).Methods("POST")
 	subrouter.HandleFunc("/samples/{id}", sampleHandler.readSample).Methods("GET")
