f78150aab7fa54a9a944b86d71804deed7f2eebc
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-08-13 09:02:40 -0400

    refactor: extracted common spans out into separate function

--- internal/monitoring/spans.go
+++ internal/monitoring/spans.go
@@ -2,27 +2,42 @@ package monitoring
 
 import (
 	"context"
+	"os"
 	"runtime"
 
 	"github.com/robert-w/go-server/internal/constants"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
+func getCommmoAttributes() []attribute.KeyValue {
+	// Set this value to 2 because this function should only be called by one of
+	// the CreateSpan functions below. Caller two will identify the function that
+	// called CreateSpan, which is what we are going for here
+	pointer, file, line, _ := runtime.Caller(2)
+
+	return []attribute.KeyValue{
+		semconv.CodeFilePath(file),
+		semconv.CodeLineNumber(line),
+		semconv.CodeFunctionName(runtime.FuncForPC(pointer).Name()),
+		semconv.ProcessRuntimeVersion(runtime.Version()),
+		semconv.ServiceName(constants.SERVICE_NAME),
+		semconv.ServiceVersion(os.Getenv("GIT_SHA")),
+	}
+}
+
 func CreateDBSpan(ctx context.Context, name string) trace.Span {
 	tracer := otel.Tracer(constants.SERVICE_NAME)
-	pointer, file, line, _ := runtime.Caller(1)
 
 	// Samplers apparently only have access to attributes provided at the time of
 	// creation. You can update the values later, but if you need to sample on an
 	// attribute, add it here
 	options := []trace.SpanStartOption{
 		trace.WithSpanKind(trace.SpanKindInternal),
+		trace.WithAttributes(getCommmoAttributes()...),
 		trace.WithAttributes(
-			semconv.CodeFilePath(file),
-			semconv.CodeLineNumber(line),
-			semconv.CodeFunctionName(runtime.FuncForPC(pointer).Name()),
 			semconv.DBCollectionName(""),
 			semconv.DBSystemNamePostgreSQL,
 			semconv.DBOperationName(""),
@@ -36,18 +51,13 @@ func CreateDBSpan(ctx context.Context, name string) trace.Span {
 
 func CreateSpan(ctx context.Context, name string) (context.Context, trace.Span) {
 	tracer := otel.Tracer(constants.SERVICE_NAME)
-	pointer, file, line, _ := runtime.Caller(1)
 
 	// Samplers apparently only have access to attributes provided at the time of
 	// creation. You can update the values later, but if you need to sample on an
 	// attribute, add it here
 	options := []trace.SpanStartOption{
 		trace.WithSpanKind(trace.SpanKindInternal),
-		trace.WithAttributes(
-			semconv.CodeFilePath(file),
-			semconv.CodeLineNumber(line),
-			semconv.CodeFunctionName(runtime.FuncForPC(pointer).Name()),
-		),
+		trace.WithAttributes(getCommmoAttributes()...),
 	}
 
 	return tracer.Start(ctx, name, options...)
