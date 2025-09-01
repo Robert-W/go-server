736eaef985b6499884aa05da6ae301691a72e750
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-09-01 00:01:04 -0400

    feat: configured pool with some default settings

--- internal/database/pools.go
+++ internal/database/pools.go
@@ -3,12 +3,26 @@ package database
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func NewPool(ctx context.Context) (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Set some sensible defaults, these can always be overriden at a later date
+	// May require some testing to figure out the best values here
+	config.MaxConnIdleTime = 10 * time.Minute
+	config.MaxConnLifetime = 30 * time.Minute
+	config.MaxConns = 8
+	config.MinConns = 4
+	config.MinIdleConns = 4
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		return nil, err
 	}
